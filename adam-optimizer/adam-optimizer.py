8085baf006c1d7f24e5c65e4d2ec723a1edea4f9
ArkaAIML <arkabanerjee2024@gmail.com>
2026-05-15 09:57:06 +0530

    Update solution for adam-optimizer

--- adam-optimizer/adam-optimizer.py
+++ adam-optimizer/adam-optimizer.py
@@ -2,22 +2,17 @@ import numpy as np
 
 def adam_step(param, grad, m, v, t,
               lr=1e-3, beta1=0.9, beta2=0.999, eps=1e-8):
+    param= np.array(param)
+    grad=np.array(grad)
+    m=np.array(m)
+    v=np.array(v)
+    m_comp=beta1*m+(1-beta1)*grad
+    v_comp=beta2*v+(1-beta2)*grad*grad
+    m_new=m_comp/(1-beta1**t)
+    v_new=v_comp/(1-beta2**t)
+    param_new=param-lr*(m_new/(np.sqrt(v_new)+eps))
+    return (param_new,m_comp,v_comp)
 
-    # Convert to NumPy arrays
-    param = np.asarray(param, dtype=float)
-    grad = np.asarray(grad, dtype=float)
-    m = np.asarray(m, dtype=float)
-    v = np.asarray(v, dtype=float)
+   
+    
 
-    # Adam updates
-    m_new = beta1 * m + (1 - beta1) * grad
-    v_new = beta2 * v + (1 - beta2) * (grad * grad)
-
-    # Bias correction
-    m_hat = m_new / (1 - beta1 ** t)
-    v_hat = v_new / (1 - beta2 ** t)
-
-    # Parameter update
-    param_new = param - lr * m_hat / (np.sqrt(v_hat) + eps)
-
-    return (param_new, m_new, v_new)
